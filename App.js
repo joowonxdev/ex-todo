6bc66c6a44d28e3974c3f89bffa9fb70d6e5884b
DEV <dev@DEVui-MacBook.local>
2018-06-20 18:46:22 +0900

    todo

--- App.js
+++ App.js
@@ -1,21 +1,33 @@
 import React from 'react';
 import { StyleSheet, Text, View, StatusBar,Dimensions,Platform} from 'react-native';
-import { TextInput } from 'react-native-gesture-handler';
-
+import { TextInput, ScrollView } from 'react-native-gesture-handler';
+import ToDo from "./ToDo.js"
 const {height, width} = Dimensions.get("window");
 
 export default class App extends React.Component {
+  state={
+    newToDo:""
+  };
   render() {
+    const { newToDo } = this.state;
     return (
       <View style={styles.container}>
         <StatusBar barStyle="light-content" />
         <Text style={styles.title}> Ex Todo </Text>
         <View style={styles.card}>
-          <TextInput style={styles.input} placeholder={"text input"}/>
+          <TextInput style={styles.input} placeholder={"text input"} value={newToDo} onChangeText={this._controllNewToDo} placeholderTextColor={"#999"} returnKeyType={"done"} autoCorrect={false}/>
+          <ScrollView contentContainerStyle={styles.todos}>
+            <ToDo />
+            </ScrollView>
         </View>
       </View>
     );
   }
+  _controllNewToDo = text =>{
+    this.setState({
+      newToDo:text
+    });
+  };
 }
 
 const styles = StyleSheet.create({
@@ -50,5 +62,14 @@ const styles = StyleSheet.create({
         elevation:3
       }
     })
+  },
+  input:{
+    padding:20,
+    borderBottomColor:"#bbb",
+    borderBottomWidth:1,
+    fontSize:25,
+  },
+  todos:{
+    alignItems:"center"
   }
 });
